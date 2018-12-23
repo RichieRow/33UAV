991edd0cda341677b204f9da9b1859f296e8decf
Richie Row <luoziyan@hotmail.com>
2018-12-23 18:50:52 +0800

    Initial commit

---
+++ app/src/main/java/com/gkd/richierow/a33uav/MainActivity.java
@@ -0,0 +1,20 @@
+package com.gkd.richierow.a33uav;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        WebView webView=(WebView)findViewById(R.id.top_center_web);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        //确保跳转到另一个网页时仍然在当前WebView显示
+        webView.loadUrl("http://www.baidu.com");
+    }
+}
